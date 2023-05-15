1e261386561d54083899201ead893edf90a6ad91
Lxst84 <126121621+Lxst84@users.noreply.github.com>
2023-05-15 12:11:03 +1000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, main.py

--- main.py
+++ main.py
@@ -7,7 +7,28 @@ scene.on_overlap_tile(SpriteKind.player,
     sprites.dungeon.purple_outer_north_west,
     on_overlap_tile)
 
-def on_overlap_tile2(sprite8, location8):
+def on_overlap_tile2(sprite42, location42):
+    tiles.set_current_tilemap(tilemap("""
+        level3
+    """))
+    tiles.place_on_tile(myCorg, tiles.get_tile_location(1, 4))
+    game.splash("level 2 complete")
+    game.splash("level 3")
+scene.on_overlap_tile(SpriteKind.player,
+    sprites.dungeon.green_inner_south_west,
+    on_overlap_tile2)
+
+def on_combos_attach_combo():
+    game.splash("ahh you've used the Kunami code  ")
+    game.splash("well have a free win")
+    controller.combos.set_timeout(1)
+    tiles.place_on_tile(myCorg, tiles.get_tile_location(8, 0))
+    tiles.set_current_tilemap(tilemap("""
+        level27
+    """))
+controller.combos.attach_combo("up down left right", on_combos_attach_combo)
+
+def on_overlap_tile3(sprite8, location8):
     tiles.set_current_tilemap(tilemap("""
         level2
     """))
@@ -16,13 +37,13 @@ def on_overlap_tile2(sprite8, location8):
     game.splash("level 2")
 scene.on_overlap_tile(SpriteKind.player,
     sprites.dungeon.collectible_insignia,
-    on_overlap_tile2)
+    on_overlap_tile3)
 
-def on_overlap_tile3(sprite, location):
+def on_overlap_tile4(sprite, location):
     game.game_over(True)
 scene.on_overlap_tile(SpriteKind.player,
     sprites.dungeon.floor_dark_diamond,
-    on_overlap_tile3)
+    on_overlap_tile4)
 
 def on_a_pressed():
     global projectile_2
@@ -49,34 +70,31 @@ def on_a_pressed():
         0)
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
-def on_overlap_tile4(sprite2, location2):
-    tiles.set_current_tilemap(tilemap("""
-        level13
-    """))
-    tiles.place_on_tile(myCorg, tiles.get_tile_location(1, 4))
-    game.splash("level 4 complete")
-    game.splash("level 5")
-scene.on_overlap_tile(SpriteKind.player,
-    sprites.dungeon.purple_outer_west2,
-    on_overlap_tile4)
-
 def on_countdown_end():
     game.game_over(False)
 info.on_countdown_end(on_countdown_end)
 
 def on_overlap_tile5(sprite7, location7):
-    info.change_life_by(-1)
-    scene.camera_shake(2, 500)
+    scene.camera_shake(4, 500)
     tiles.place_on_tile(myCorg, tiles.get_tile_location(1, 4))
+    statusbar.value += -10
 scene.on_overlap_tile(SpriteKind.player,
     sprites.dungeon.hazard_lava1,
     on_overlap_tile5)
 
-def on_life_zero():
+def on_on_zero(status):
     game.game_over(False)
-info.on_life_zero(on_life_zero)
+statusbars.on_zero(StatusBarKind.health, on_on_zero)
 
-def on_overlap_tile6(sprite5, location5):
+def on_overlap_tile6(sprite2, location2):
+    game.game_over(True)
+scene.on_overlap_tile(SpriteKind.player,
+    assets.tile("""
+        myTile7
+    """),
+    on_overlap_tile6)
+
+def on_overlap_tile7(sprite5, location5):
     tiles.place_on_tile(myCorg, tiles.get_tile_location(1, 5))
     game.splash("level 5 complete")
     game.splash("level 6")
@@ -85,17 +103,6 @@ def on_overlap_tile6(sprite5, location5):
     """))
 scene.on_overlap_tile(SpriteKind.player,
     sprites.dungeon.purple_outer_south2,
-    on_overlap_tile6)
-
-def on_overlap_tile7(sprite4, location4):
-    tiles.set_current_tilemap(tilemap("""
-        level3
-    """))
-    tiles.place_on_tile(myCorg, tiles.get_tile_location(1, 4))
-    game.splash("level 2 complete")
-    game.splash("level 3")
-scene.on_overlap_tile(SpriteKind.player,
-    sprites.dungeon.green_inner_south_west,
     on_overlap_tile7)
 
 def on_overlap_tile8(sprite6, location6):
@@ -109,8 +116,25 @@ scene.on_overlap_tile(SpriteKind.player,
     sprites.dungeon.purple_outer_south_east,
     on_overlap_tile8)
 
+def on_overlap_tile9(sprite22, location22):
+    tiles.set_current_tilemap(tilemap("""
+        level13
+    """))
+    tiles.place_on_tile(myCorg, tiles.get_tile_location(1, 4))
+    game.splash("level 4 complete")
+    game.splash("level 5")
+scene.on_overlap_tile(SpriteKind.player,
+    sprites.dungeon.purple_outer_west2,
+    on_overlap_tile9)
+
 projectile_2: Sprite = None
 myCorg: Corgio = None
+statusbar: StatusBarSprite = None
+statusbar = statusbars.create(20, 4, StatusBarKind.health)
+statusbar.set_label("HP")
+statusbar.attach_to_sprite(myCorg)
+statusbar.position_direction(CollisionDirection.TOP)
+statusbar.set_status_bar_flag(StatusBarFlag.SMOOTH_TRANSITION, True)
 game.splash("to move press w a s d or the arrow keys")
 controller.move_sprite(myCorg, 100, 0)
 myCorg = corgio.create(SpriteKind.player)
@@ -121,5 +145,3 @@ tiles.set_tilemap(tilemap("""
     level1
 """))
 scene.camera_follow_sprite(myCorg)
-info.set_life(10)
-info.start_countdown(90)
--- main.ts
+++ main.ts
@@ -2,6 +2,12 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.purpleOuterNorthWest, fun
     tiles.placeOnTile(myCorg, tiles.getTileLocation(1, 4))
     tiles.setCurrentTilemap(tilemap`level8`)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenInnerSouthWest, function (sprite42, location42) {
+    tiles.setCurrentTilemap(tilemap`level3`)
+    tiles.placeOnTile(myCorg, tiles.getTileLocation(1, 4))
+    game.splash("level 2 complete")
+    game.splash("level 3")
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite8, location8) {
     tiles.setCurrentTilemap(tilemap`level2`)
     tiles.placeOnTile(myCorg, tiles.getTileLocation(1, 4))
@@ -9,7 +15,10 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, func
     game.splash("level 2")
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.floorDarkDiamond, function (sprite, location) {
-    game.gameOver(true)
+    game.splash("level 7 complete")
+    game.splash("level 8")
+    tiles.placeOnTile(myCorg, tiles.getTileLocation(1, 6))
+    tiles.setCurrentTilemap(tilemap`level29`)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile_2 = sprites.createProjectileFromSprite(img`
@@ -31,24 +40,26 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         `, myCorg, 200, 0)
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.purpleOuterWest2, function (sprite2, location2) {
-    tiles.setCurrentTilemap(tilemap`level13`)
-    tiles.placeOnTile(myCorg, tiles.getTileLocation(1, 4))
-    game.splash("level 4 complete")
-    game.splash("level 5")
-})
 info.onCountdownEnd(function () {
     game.gameOver(false)
 })
+controller.combos.attachCombo("up down left right  ", function () {
+    game.splash("ahh you've used the Kunami code  ")
+    game.splash("well have a free win")
+    controller.combos.setTimeout(1)
+    tiles.placeOnTile(myCorg, tiles.getTileLocation(8, 0))
+    tiles.setCurrentTilemap(tilemap`level27`)
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite7, location7) {
-    scene.cameraShake(2, 500)
+    scene.cameraShake(4, 500)
     tiles.placeOnTile(myCorg, tiles.getTileLocation(1, 4))
+    statusbar.value += -10
 })
 statusbars.onZero(StatusBarKind.Health, function (status) {
     game.gameOver(false)
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
-    statusbar.value += -10
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite2, location2) {
+    game.gameOver(true)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.purpleOuterSouth2, function (sprite5, location5) {
     tiles.placeOnTile(myCorg, tiles.getTileLocation(1, 5))
@@ -56,17 +67,17 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.purpleOuterSouth2, functi
     game.splash("level 6")
     tiles.setCurrentTilemap(tilemap`level22`)
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenInnerSouthWest, function (sprite4, location4) {
-    tiles.setCurrentTilemap(tilemap`level3`)
-    tiles.placeOnTile(myCorg, tiles.getTileLocation(1, 4))
-    game.splash("level 2 complete")
-    game.splash("level 3")
-})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.purpleOuterSouthEast, function (sprite6, location6) {
     tiles.placeOnTile(myCorg, tiles.getTileLocation(1, 4))
     tiles.setCurrentTilemap(tilemap`level8`)
     game.splash("level 3 complete")
     game.splash("level 4")
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.purpleOuterWest2, function (sprite22, location22) {
+    tiles.setCurrentTilemap(tilemap`level13`)
+    tiles.placeOnTile(myCorg, tiles.getTileLocation(1, 4))
+    game.splash("level 4 complete")
+    game.splash("level 5")
 })
 let projectile_2: Sprite = null
 let myCorg: Corgio = null
